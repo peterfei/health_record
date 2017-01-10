af798762906588e14fbd5cbe76ddecd284b11f9d
peterfei <peterfeispace@gmail.com>
2017-01-10 15:40:12 +0800

    Fixed Welcome Index Error

--- app/controllers/wechat_api_controller.rb
+++ app/controllers/wechat_api_controller.rb
@@ -1,11 +1,6 @@
-class WelcomeController < ApplicationController
+class WechatApiController <  ActionController::Base
 	wechat_api
-	def index
-		@total_users = User.all.count rescue 0
-		@new_users = User.where("create_at > ?", Time.now.at_beginning_of_day).count rescue 0
-		@total_health_items = HealthItem.all.count rescue 0
-		@new_health_items = HealthItem.where("create_at > ?", Time.now.at_beginning_of_day).count rescue 0
-	end
+
 
 	def text
 		template = YAML.load(File.read("#{Rails.public_path}/tem.yml"))
