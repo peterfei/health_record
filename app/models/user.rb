a692158eab39bb329ee1a9f80954fb0e0a712a70
guo xiaofeng <1810014005@qq.com>
2017-01-12 14:11:01 +0800

    更改个人信息接口变更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 	has_many :medical_record_managements
 	has_many :take_medicine_managements
 
-	acts_as_taggable_on :hobby, :job
+	acts_as_taggable_on :hobby, :job, :speciality, :skill_level
 	enum sex: [:male, :female]
 	enum id_type: [:identity, :passport, :officer, :other_id]
 	enum blood_type: [:type_a, :type_b, :type_ab, :type_o, :type_other]
