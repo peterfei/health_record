0c48cfa80e6b62540f94dd463bc5354db61cf841
guo xiaofeng <1810014005@qq.com>
2017-02-17 15:03:21 +0800

    解除请求关注时推送莫办消息

--- app/controllers/api/v1/user_focus.rb
+++ app/controllers/api/v1/user_focus.rb
@@ -42,7 +42,7 @@ module API
 									error!('已申请关注或已关注成功')
 								else
 									if UserFocu.create! appellation: params[:appellation], follow_id: follow_id, user_id: @current_user.id, whether: 0
-										#send_temp_message(user,@current_user)
+										send_temp_message(user,@current_user)
 										{ status: :ok }
 									else
 										error!('保存失败')
